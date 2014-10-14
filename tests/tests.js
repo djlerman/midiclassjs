7c76ce923e4ae14618044d2877381a085b648289
Rick Hutcheson <rick@oddlyaccurate.com>
2014-10-14 17:55:33 -0400

    adds test bootstrap files

---
+++ tests/tests.js
@@ -0,0 +1,7 @@
+QUnit.test( "library load", function( assert ) {
+  assert.ok( MIDITools, "library loaded!" );
+});
+
+QUnit.test( "load simple MIDI", function( assert ) {
+  assert.ok( MIDITools, "library loaded!" );
+});
