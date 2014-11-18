d9a1698ecceef17dad4461023115b653d0fe755e
Rick Hutcheson <rick@oddlyaccurate.com>
2014-11-18 15:53:38 -0500

    Update documentation for MIDIFile

--- js/MIDIFile.js
+++ js/MIDIFile.js
@@ -56,6 +56,8 @@ MIDIFile.prototype.track = function(n) {
 /**
  * Adds a new, empty track to the file, then returns the track.
  * @returns {MIDITools.MIDITrack} the newly-added track
+ * @throws errors.midi.trackOverflow if the file already has
+ *         the maximum number of tracks
  */
 
 MIDIFile.prototype.addTrack = function() {
@@ -71,12 +73,21 @@ MIDIFile.prototype.addTrack = function() {
   return this._tracks[this._tracks.length-1];
 };
 
+
+/**
+ * Removes the track at index `n` from the file
+ * @throws errors.midi.removeInvalidTrack if the track does
+ *         not exist in this file
+ */
+
 MIDIFile.prototype.removeTrack = function(n) {
   if (this._tracks.length <= n) {
     throw errors.midi.removeInvalidTrack;
   }
   this._tracks.splice(n, 1);
 };
+
+
 /**
  * Returns the number of tracks in the MIDIFile.
  * @returns {Number} total number of tracks stored in the MIDIFile
@@ -84,11 +95,6 @@ MIDIFile.prototype.removeTrack = function(n) {
 
 MIDIFile.prototype.countTracks = function() {
   return this._tracks.length;
-};
-
-
-MIDIFile.prototype.exportBase64 = function() {
-  return 'base64,' + window.btoa(this.exportBinary());
 };
 
 
@@ -104,6 +110,17 @@ MIDIFile.prototype.getTiming = function() {
 };
 
 
+/**
+ * Sets the MIDI file's header field for time divisions.
+ * @param {Number|PlainObject} If `timing` is a number, 
+ *        then the MIDIFile's division type will be set to
+ *        `ticksPerBeat` and the number will be used as the
+ *        value of this field. If `timing` is an object, 
+ *        then it must have the properties `framesPerSecond` and
+ *        `ticksPerFrame`, and these will be used to set the 
+ *        time division as documented in ???
+ * TODO: add reference
+ */
 MIDIFile.prototype.setTiming = function(timing) {
   var hasFrameParameters = (
     typeof timing === 'object' &&
@@ -124,11 +141,11 @@ MIDIFile.prototype.setTiming = function(timing) {
   }
 };
 
-function toByteString(str) {
-  return Array.prototype.map.call(str, function(ch) {
-    return (0x00FF & ch.charCodeAt(0));
-  });
-}
+
+MIDIFile.prototype.exportBase64 = function() {
+  return 'base64,' + window.btoa(this.exportBinary());
+};
+
 
 MIDIFile.importBinary = function(src, callback, error) {
   if (!src || !callback) {
@@ -152,6 +169,13 @@ MIDIFile.importBinary = function(src, callback, error) {
   });
 };
 
+
+
+function toByteString(str) {
+  return Array.prototype.map.call(str, function(ch) {
+    return (0x00FF & ch.charCodeAt(0));
+  });
+}
 
 MIDIFile.importText = function(text) {
   return importers.text(new MIDIFile(), text);
