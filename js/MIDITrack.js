3c256fc07e42ea0d9ba61321dca7cef608b98051
Rick Hutcheson <rick@oddlyaccurate.com>
2015-01-09 00:38:04 -0500

    Add documentation to MIDITrack API

--- js/MIDITrack.js
+++ js/MIDITrack.js
@@ -11,6 +11,8 @@ function MIDITrack() {
 
 /**
  * Returns the event object at index `i`.
+ * 
+ * @method event
  * @returns {Event} the `i`th event object in this track
  */
 
@@ -20,12 +22,12 @@ MIDITrack.prototype.event = function(i) {
 
 
 /**
- * Adds the given event to this track. Checks
- * for all required parameters for the event's type
- * and throws an error if any are missing. Furthermore,
- * defines index properties for these parameters in their defined
- * ordering as per the MIDI specification.
+ * Adds the given event to this track. Checks for all required
+ * parameters for the event's type and throws an error if any are
+ * missing. Furthermore, named index properties are also defined at
+ * their appropriate index per the MIDI specification.
  *
+ * @method addEvent
  * @throws {error.track.parameterMissing} if an event parameter
  *         is missing
  */
@@ -34,24 +36,43 @@ MIDITrack.prototype.addEvent = function(evt) {
   this._events.push(checkEvent(evt));
 };
 
+
 /**
- * @todo
+ * Adds all elements in `evts` to this track. Performs same checks as
+ * `addEvent()`.
+ *
+ * @method addEvents
+ * @throws {error.track.parameterMissing} if an event parameter
+ *         is missing
+ * @see addEvent
  */
+
 MIDITrack.prototype.addEvents = function(evts) {
   this._events = this._events.concat(evts.map(checkEvent));
 };
+
 
 /**
  * Replaces the event at index `i` with `evt`; `evt`'s
  * parameters are checked as if it were added
  * with `addEvent`. Throws an error if the parameters
  * are invalid, and preserves current track events.
+ * 
+ * @method replaceEvent
+ *
+ * @param {Number} i
+ * index of the event to replace
+ * 
+ * @param {Object} evt
+ * MIDI event to insert into the track
  */
+
 MIDITrack.prototype.replaceEvent = function(i, evt) {
   this._events.splice(i, 1, checkEvent(evt));
 };
 
 
+// Helper function; checks event parameters
 function checkEvent(evt) {
   var spec = data.typeMap[evt.message];
   evt.kind = spec.kind;
@@ -59,8 +80,7 @@ function checkEvent(evt) {
   var checked = {};
   if (spec.length === 'variable') {
     checked.value = evt.parameters.value;
-  }
-  else {
+  } else {
     spec.parameters.forEach(function(p, index) {
       if (evt.parameters[p.name] === 'undefined') {
   throw errors.track.parameterMissing;
@@ -76,7 +96,9 @@ function checkEvent(evt) {
 
 
 /**
- * Returns the number of events stored in this track
+ * Returns the number of events stored in this track.
+ *
+ * @method countEvents
  * @return {Number} the number of events stored in this track
  */
 
@@ -85,9 +107,19 @@ MIDITrack.prototype.countEvents = function(trackNumber) {
 };
 
 
-MIDITrack.prototype.filterEvents = function(type) {
+/**
+ * Returns all events in the track whose `message` property matches
+ * `msg`. Message types include `noteOn`, `programChange`, etc.
+ *
+ * @method filterEvents
+ *
+ * @param {string} msg
+ * The message type by which to filter events
+ */
+
+MIDITrack.prototype.filterEvents = function(msg) {
   return this._events.filter(function(evt) {
-    return (evt.message === type);
+    return (evt.message === msg);
   });
 };
 
