c5efc4ec4f9428b819bcc0b1f4b13a1d9ff8703a
Rick Hutcheson <rick@oddlyaccurate.com>
2016-03-01 21:00:20 -0800

    Refactor MIDISequence tests with beforeEach()

--- spec/midisequence.spec.js
+++ spec/midisequence.spec.js
@@ -2,14 +2,18 @@ var mt = require('../js/index');
 var errors = require('../js/errors');
 
 describe('MIDISequence', function() {
+  var midi;
+
+  beforeEach(function() {
+    midi = new mt.createSequence();
+  });
+
   describe('countBeats', function() {
     it('should return 0 for an empty sequence', function() {
-      var midi = new mt.createSequence();
       expect(midi.countBeats()).toBe(0);
     });
 
     it('should return 1 for a single beat', function() {
-      var midi = new mt.createSequence();
       midi.channel(0).addEvent(0, 'noteOn', {
         note: 0x3C,
         velocity: 0x60
@@ -22,8 +26,6 @@ describe('MIDISequence', function() {
     });
 
     it('should correctly count a several-beat passage', function() {
-      var midi = new mt.createSequence();
-
       midi.channel(0).addEvent(0, 'noteOn', {
         note: 0x3C,
         velocity: 0x60
@@ -53,7 +55,6 @@ describe('MIDISequence', function() {
     });
 
     it('should correctly count beats given overlapping notes', function() {
-      var midi = new mt.createSequence();
       midi.channel(0).addEvent(0, 'noteOn', {
         note: 0x3C,
         velocity: 0x60
@@ -86,7 +87,6 @@ describe('MIDISequence', function() {
   }); // countBeats
 
   describe('channel', function() {
-    var midi = new mt.createSequence();
     it('should return channel i, given number i= 0--15', function() {
       for (var i = 0, n = 15; i < n; i += 1) {
         expect(midi.channel(i).number()).toBe(i);
@@ -94,21 +94,18 @@ describe('MIDISequence', function() {
     });
 
     it('should throw an error for i < 0', function() {
-      var midi = new mt.createSequence();
-      expect(function() {
+            expect(function() {
         midi.channel(-1);
       }).toThrow(errors.general.channelRange);
     });
 
     it('should throw an error for i > 15', function() {
-      var midi = new mt.createSequence();
-      expect(function() {
+            expect(function() {
         midi.channel(16);
       }).toThrow(errors.general.channelRange);
     });
 
     it('should return channels which are on distinct tracks', function() {
-      var midi = new mt.createSequence();
       var channels = [];
       for (var i = 0, n = 15; i < n; i += 1) {
         channels.push(midi.channel(i));
@@ -131,12 +128,10 @@ describe('MIDISequence', function() {
 
   describe('usedChannels', function() {
     it('should return 0 for a newly-initialized MIDISequence', function() {
-      var midi = mt.createSequence();
       expect(midi.usedChannels()).toEqual([]);
     });
 
     it('should return 1 for sequences with single event', function() {
-      var midi = mt.createSequence();
       midi.channel(0).addEvent(0, 'noteOn', {
         note: 0x3C,
         velocity: 0x60
@@ -145,7 +140,6 @@ describe('MIDISequence', function() {
     });
 
     it('should return correct value for multi-channel sequences', function() {
-      var midi = mt.createSequence();
       midi.channel(0).addEvent(0, 'noteOn', { note: 0x3C, velocity: 0x60 });
       midi.channel(0).addEvent(95, 'noteOff', { note: 0x3C, velocity: 0x60 });
       midi.channel(1).addEvent(0, 'noteOn', { note: 0x3C, velocity: 0x60 });
@@ -154,4 +148,21 @@ describe('MIDISequence', function() {
     });
   });
 
-}); // MIDISequence
+  describe('getTempo', function() {
+    it('should return 120 for a newly-initialized MIDISequence', function() {
+      expect(midi.getTempo()).toEqual(120);
+    });
+
+    it('should return previously-set value if sequence tempo set', function() {
+      midi.setTempo(240);
+      expect(midi.getTempo()).toEqual(240);
+    });
+  });
+
+  describe('setTempo', function() {
+    it('should set values within standard range', function() {
+      midi.setTempo(240);
+      expect(midi.getTempo()).toEqual(240);
+    });
+  });
+});
